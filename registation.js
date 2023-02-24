f12c0db487a30728553bd84a1d5b379b2333a9cb
SubrataOvaku <subratab@ovaku.in>
2023-02-24 15:46:27 +0530

    add validation on onclick button,edite button

--- registation.js
+++ registation.js
@@ -1,28 +1,39 @@
-//global variable
-var x;
-var m;
-var z;
-
-
-//for delete are row
-// function DeleteRecord(id) {
-//     console.log(id);
-//     // console.log(document.getElementById("tableid"));
-//     document.getElementById("tableid").deleteRow(id);
-    
-//   }
 
 //for display details
 function displayDetails(){
-    var id = document.getElementById("sl").value;
-    var name = document.getElementById("name").value;
-    var number = document.getElementById("number").value;
-    var email = document.getElementById("email").value;
-    var city = document.getElementById("city").value;
-    var state = document.getElementById("state").value;
-    var country = document.getElementById("country").value;
-    var pin = document.getElementById("pin").value;
-
+ 
+  if(valId()){
+    var id = document.getElementById("id").value;
+  }
+  name1=document.getElementById("name");
+  // if(valName(name)){
+  //   var name = name.value;
+  // }  
+  number = document.getElementById("number");
+  // if(valNumber(number)){
+  //   var number = number.value;
+  // }
+  email =document.getElementById("email");
+  // if( valEmail(email)){
+  //   var email = email.value;
+  // }
+  city = document.getElementById("city")
+  // if(valCity(city)){
+  //   var city = city.value;
+  // } 
+  state = document.getElementById("state");
+  // if(valState(state)){
+  //   var state = state.value;
+  // }
+  country = document.getElementById("country");
+  // if(valCountry(country)){
+  // var country = country.value;
+  // }
+  pin = document.getElementById("pin");
+  // if(valPin(pin)){
+  // var pin = pin.value;
+  // }
+   if(valAll(name1,number,email,city,state,country,pin)){
     var takeid = document.getElementById("tableid"); 
     var row = document.createElement("tr");
     takeid.appendChild(row);   
@@ -39,59 +50,90 @@ function displayDetails(){
   
 
     col0.innerHTML = id;
-    col1.innerHTML = name;
-    col2.innerHTML = number;
-    col3.innerHTML = email;
-    col4.innerHTML = city;
-    col5.innerHTML = state;
-    col6.innerHTML = country;
-    col7.innerHTML = pin;
-    col8.innerHTML = '<button class="btn edit" data-bs-toggle="modal" data-bs-target="#editModal" onclick="myf(this); showeditevalue();"><i class="fas fa-edit"></i></button><button class="btn delete" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="myf(this)"><i class="fas fa-trash-alt" style="color: red "></i></button>';   
-} 
-function myf(i){
+    col1.innerHTML = name1.value;
+    col2.innerHTML = number.value;
+    col3.innerHTML = email.value;
+    col4.innerHTML = city.value;
+    col5.innerHTML = state.value;
+    col6.innerHTML = country.value;
+    col7.innerHTML = pin.value;
+    col8.innerHTML = '<button class="btn edit" data-bs-toggle="modal" data-bs-target="#editModal" onclick="rowDetalis(this); showEditeValue();"><i class="fas fa-edit"></i></button><button class="btn delete" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="rowDetalis(this)"><i class="fas fa-trash-alt" style="color: red "></i></button>';   
+  } 
+  
+ }
+//global variable
+var x;
+var m;
+var z;
+
+function rowDetalis(i){
   x=i;
+
 }
 function deleteRow() {
  
   var row = x.parentNode.parentNode;
   row.parentNode.removeChild(row);
+ 
   var toast = document.getElementById('dtoast');
   var toastObj = new bootstrap.Toast(toast);
   toastObj.show();
 }
 //add table edite details
-function myedite(){
+function editeRow(){
  
   var row = x.parentNode.parentNode;
   var id = document.getElementById("eid").value;
-  var name = document.getElementById("ename").value;
-  var number = document.getElementById("enumber").value;
-  var email = document.getElementById("eemail").value;
-  var city = document.getElementById("ecity").value;
-  var state = document.getElementById("estate").value;
-  var country = document.getElementById("ecountry").value;
-  var pin = document.getElementById("epin").value;
+  name1 = document.getElementById("ename");
+  // if(valName(name)){
+  //   var name = name.value;
+  // }
+  number = document.getElementById("enumber");
+  // if(valNumber(number)){
+  //   var number = number.value;
+  // }
+  email =document.getElementById("eemail");
+  // if( valEmail(email)){
+  //   var email = email.value;
+  // }
+  city = document.getElementById("ecity");
+  // if(valCity(city)){
+  //   var city = city.value;
+  // } 
+  state = document.getElementById("estate");
+  // if(valState(state)){
+  //   var state = state.value;
+  // }
+  country = document.getElementById("ecountry");
+  // if(valCountry(country)){
+  // var country = country.value;
+  // }
+  pin = document.getElementById("epin");
+  // if(valPin(pin)){
+  // var pin = pin.value;
+  // }
  
- 
+  if(valAll(name1,number,email,city,state,country,pin)){
   row.childNodes[0].innerHTML = id;
-  row.childNodes[1].innerHTML = name;
-  row.childNodes[2].innerHTML = number;
-  row.childNodes[3].innerHTML = email;
-  row.childNodes[4].innerHTML = city;
-  row.childNodes[5].innerHTML = state;
-  row.childNodes[6].innerHTML = country;
-  row.childNodes[7].innerHTML = pin;
-  row.childNodes[8].innerHTML = '<button class="btn edit" data-bs-toggle="modal" data-bs-target="#editModal" onclick="myf(this) showeditevalue()"><i class="fas fa-edit"></i></button><button class="btn delete" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="myf(this)"><i class="fas fa-trash-alt" style="color: red "></i></button>';
+  row.childNodes[1].innerHTML = name1.value;
+  row.childNodes[2].innerHTML = number.value;
+  row.childNodes[3].innerHTML = email.value;
+  row.childNodes[4].innerHTML = city.value;
+  row.childNodes[5].innerHTML = state.value;
+  row.childNodes[6].innerHTML = country.value;
+  row.childNodes[7].innerHTML = pin.value;
+  row.childNodes[8].innerHTML = '<button class="btn edit" data-bs-toggle="modal" data-bs-target="#editModal" onclick="rowDetalis(this); showEditeValue();"><i class="fas fa-edit"></i></button><button class="btn delete" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="rowDetalis(this)"><i class="fas fa-trash-alt" style="color: red "></i></button>';
   var toast1 = document.getElementById("etoast");
-  console.log(toast1);
+  // console.log(toast1);
   var toastObj = new bootstrap.Toast(toast1);
   toastObj.show();
+  }
     
 } 
-function showeditevalue(){
-  var row = x.parentNode.parentNode;
-
-  var id = (document.getElementById("eid").value = row.childNodes[0].innerHTML);
+function showEditeValue(){
+    var row = x.parentNode.parentNode;
+    
+    var id = (document.getElementById("eid").value = row.childNodes[0].innerHTML);
     var name = (document.getElementById("ename").value = row.childNodes[1].innerHTML);
     var mobileNumber = (document.getElementById("enumber").value = row.childNodes[2].innerHTML);
     var email = (document.getElementById("eemail").value = row.childNodes[3].innerHTML);
---
+++ validation.js
@@ -0,0 +1,141 @@
+/*
+*@validation 
+**Add new function for validation all fild and submit button
+*/
+//validation for id
+function valId(){   
+    var e = document.getElementById("tableid");
+    var k = e.getElementsByTagName("tr");
+    console.log(k);
+    for (let i = 0; i <= e.getElementsByTagName("tr"); i++) {
+        
+        
+    }
+    var  id = document.getElementById("id").value;
+    //console.log(id);
+    if (/^\d{1,2}$/.test(id)) {
+        return true;
+    } else {
+        alert("WRONG");
+        id = null;
+        return false;
+    } 
+   
+}
+
+
+/*
+@valName
+** use for name validation
+** variable: name;
+*/
+function valName(validName) {
+    var div = validName.parentNode;
+    var p = div.querySelector("#valName");
+    validName = validName.value;
+    if (/^[a-zA-Z]+([ ][a-zA-Z]+)*$/.test(validName)) {
+        p.innerHTML = null;
+        return true;
+    }
+    else {
+        p.innerHTML = "invalid name";
+        return false;
+    }
+}
+
+function valNumber(validNumber) {
+    var div = validNumber.parentNode;
+    console.log(validNumber.parentNode);
+    console.log(validNumber.parentNode.querySelector("#valNumber"));
+    var p = div.querySelector("#valNumber");
+    validNumber = validNumber.value;
+    if (/^\d{10}$/.test(validNumber)) {
+        p.innerHTML = null;
+        return true;
+    }
+    else {
+        p.innerHTML = "Invalid phone number";
+        return false;
+    }
+}
+
+function valEmail(validEmail){
+    var div = validEmail.parentNode;
+    var p = div.querySelector("#valEmail");
+    validEmail = validEmail.value;
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(validEmail)) {
+        p.innerHTML = null;
+        return true;
+    }
+    else {
+        p.innerHTML = "Invalid EmailId";
+        return false;
+    }
+   
+}
+function valCity(validCity){
+    var div = validCity.parentNode;
+    var p = div.querySelector("#valcity");
+    validCity = validCity.value;
+    if (validCity=="") {        
+        p.innerHTML = "Enter your city";
+        return false;
+    }
+    else {
+        p.innerHTML = null;
+        return true;
+    }
+   
+}
+function valState(validState){
+    var div = validState.parentNode;
+    var p = div.querySelector("#valState");
+    validState = validState.value;
+    if ( validState=="") {
+        
+        p.innerHTML = "Enter your state";
+        return false;
+    }
+    else {
+        p.innerHTML = null;
+        return true;
+    }
+   
+}
+function valCountry(validCountry){
+    var div = validCountry.parentNode;
+    var p = div.querySelector("#valCountry");
+    validCountry = validCountry.value;
+    if (/^[a-zA-Z]+([ ][a-zA-Z]+)*$/.test(validCountry)) {
+       p.innerHTML = null;
+        return true;
+    }
+    else {
+        p.innerHTML = "Enter your country";
+        return false;
+    }
+   
+}
+
+function valPin(validPin){
+    var div = validPin.parentNode;
+    var p = div.querySelector("#valPin");
+    validPin = validPin.value;
+    if (/^\d{5,6}$/.test(validPin)) {
+       p.innerHTML = null;
+        return true;
+    }
+    else {
+        p.innerHTML = "Enter your Pincode";
+        return false;
+    }
+   
+}
+
+function valAll(name,number,email,city,state,country,pin) {
+     if (valName(name) && valNumber(number) && valEmail(email) && valCity(city) && valState(state) && valCountry(country) && valPin(pin)) {
+     return true;    
+     }else {  
+         return false; 
+     }
+}
