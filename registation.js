37663e715561f51dd49c10a29f962fdfd94579a9
SubrataOvaku <subratab@ovaku.in>
2023-03-02 18:15:21 +0530

    code optimise and delete dead code

--- registation.js
+++ registation.js
@@ -1,18 +1,24 @@
+/*
+@getValueOfApi
+** call this function for show data in table
+*/
+getValueOfAPi();
 
 /* 
 @this function is to display all Employee Details
 @ Ade variable:
 */
-function displayDetails(){
-  name1=document.getElementById("name"); 
-  number = document.getElementById("number");
-  email =document.getElementById("email");
-  city = document.getElementById("city");
-  state = document.getElementById("state");
-  country = document.getElementById("country");
-  pin = document.getElementById("pin");
-    if(valAll(name1,number,email,city,state,country,pin)){   
+function displayDetails() {
+    name1 = document.getElementById("name"); 
+    number = document.getElementById("number");
+    email = document.getElementById("email");
+    city = document.getElementById("city");
+    state = document.getElementById("state");
+    country = document.getElementById("country");
+    pin = document.getElementById("pin");
+    if(valAll(name1,number,email,city,state,country,pin)) {   
         datas = {
+          // @todo use variable
           "name": document.getElementById("name").value,
           "phoneNo": document.getElementById("number").value,
           "email": document.getElementById("email").value,
@@ -22,19 +28,19 @@ function displayDetails(){
           "pin": document.getElementById("pin").value,
         }
         var url = "http://192.168.0.107:8080/students";
-        var pa = {
-          method:"POST",
-          headers:{
-            "content-Type":"application/json"
-          },
-          body:JSON.stringify(datas),
-          }
-          fetch(url,pa).then(response =>{
+        var param = {
+            method:"POST",
+            headers:{
+                "content-Type":"application/json"
+            },
+            body:JSON.stringify(datas),
+        }
+        fetch(url,param).then(response =>{
             return response.json();
-          }).then(data => {
-            var takeid = document.getElementById("api"); 
+        }).then(data => {
+            var table = document.getElementById("tableBody");
             var row = document.createElement("tr");
-            takeid.appendChild(row); 
+            table.appendChild(row); 
             var col0 = row.appendChild(document.createElement("td"));
             var col1 = row.appendChild(document.createElement("td"));
             var col2 = row.appendChild(document.createElement("td"));
@@ -52,87 +58,79 @@ function displayDetails(){
             col5.innerHTML = data.state;
             col6.innerHTML = data.country;
             col7.innerHTML = data.pin;
-            col8.innerHTML = '<button class="btn edit" data-bs-toggle="modal" data-bs-target="#editModal" onclick="rowDetalis(this); showEditeValue();"><i class="fas fa-edit"></i></button><button class="btn delete" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="rowDetalis(this)"><i class="fas fa-trash-alt" style="color: red "></i></button>';   
-            
-          })
+            col8.innerHTML = '<button class="btn edit" data-bs-toggle="modal" data-bs-target="#editModal" onclick="rowDetalis(this); showEditeValue();"><i class="fas fa-edit"></i></button><button class="btn delete" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="rowDetalis(this)"><i class="fas fa-trash-alt" style="color: red "></i></button>';
+        })
     }
-  }
+}
+
 //global variable
-var x;
-var m;
-var z;
+var rowElement;
+
+
 /*
 @ this function for detealis of row
-
 */
-function rowDetalis(i){
-  x=i;
-
+function rowDetalis(element) {
+    rowElement = element;
 }
+
 /*
 @deleterow
 ** this fuction is use to delete a row 
- */
+*/
 function deleteRow() {
- 
-  var row = x.parentNode.parentNode;
-  var id = row.childNodes[0].innerHTML;
-  console.log(id);
-  var url = "http://192.168.0.107:8080/students/" + id;
-  var p = {
-  method:"DELETE",
-  headers:{
-    "Content-Type":"application/json"
-  }
-  }
-  fetch(url,p).then(response =>{  
-  var toast = document.getElementById('dtoast');
-  var toastObj = new bootstrap.Toast(toast);
-  toastObj.show();
-  row.parentNode.removeChild(row);
-  })
- 
+    var row = rowElement.parentNode.parentNode;
+    var id = row.childNodes[0].innerHTML;
+    var url = "http://192.168.0.107:8080/students/" + id;
+    var param = {
+    method:"DELETE",
+    headers:{
+        "Content-Type":"application/json"
+    }
+    }
+    fetch(url,param).then(response =>{
+        var toast = document.getElementById('dtoast');
+        var toastObj = new bootstrap.Toast(toast);
+        toastObj.show();
+        row.parentNode.removeChild(row);
+    })
 }
+
 /*
 @editeRow
 **this function is for edite row
 */
-function editeRow(){
- 
-  
-  var id = document.getElementById("eid").value;
-  name1 = document.getElementById("ename");
-  number = document.getElementById("enumber");
-  email =document.getElementById("eemail");
-  city = document.getElementById("ecity");
-  state = document.getElementById("estate");
-  country = document.getElementById("ecountry");
-  pin = document.getElementById("epin");
+function editeRow() {
+    var id = document.getElementById("eid").value;
+    name1 = document.getElementById("ename");
+    number = document.getElementById("enumber");
+    email =document.getElementById("eemail");
+    city = document.getElementById("ecity");
+    state = document.getElementById("estate");
+    country = document.getElementById("ecountry");
+    pin = document.getElementById("epin");
     if(valAll(name1,number,email,city,state,country,pin)){
-      datas = {
-        "name": document.getElementById("ename").value,
-        "phoneNo": document.getElementById("enumber").value,
-        "email": document.getElementById("eemail").value,
-        "city": document.getElementById("ecity").value,
-        "state": document.getElementById("estate").value,
-        "country": document.getElementById("ecountry").value,
-        "pin": document.getElementById("epin").value,
-  
-      }
+        datas = {
+            "name": document.getElementById("ename").value,
+            "phoneNo": document.getElementById("enumber").value,
+            "email": document.getElementById("eemail").value,
+            "city": document.getElementById("ecity").value,
+            "state": document.getElementById("estate").value,
+            "country": document.getElementById("ecountry").value,
+            "pin": document.getElementById("epin").value,
+        }
         var url = "http://192.168.0.107:8080/students/" +id ;
-        var pa = {
-          method:"put",
-          headers:{
-            "content-Type":"application/json"
-          },
-          body:JSON.stringify(datas),
-          }
-          fetch(url,pa).then(response =>{
+        var param = {
+            method:"put",
+            headers:{
+                "content-Type":"application/json"
+            },
+            body:JSON.stringify(datas),
+        }
+        fetch(url,param).then(response =>{
             return response.json();
-          }).then(data => {       
-            console.log(data);
-         
-            var row = x.parentNode.parentNode;
+        }).then(data => {
+            var row = rowElement.parentNode.parentNode;
             row.childNodes[0].innerHTML = id;
             row.childNodes[1].innerHTML = data.name;
             row.childNodes[2].innerHTML = data.phoneNo;
@@ -141,22 +139,25 @@ function editeRow(){
             row.childNodes[5].innerHTML = data.state;
             row.childNodes[6].innerHTML = data.country;
             row.childNodes[7].innerHTML = data.pin;
-            row.childNodes[8].innerHTML = '<button class="btn edit" data-bs-toggle="modal" data-bs-target="#editModal" onclick="rowDetalis(this); showEditeValue();"><i class="fas fa-edit"></i></button><button class="btn delete" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="rowDetalis(this)"><i class="fas fa-trash-alt" style="color: red "></i></button>';
-           })
-            var toast1 = document.getElementById("etoast");
-            // console.log(toast1);
-            var toastObj = new bootstrap.Toast(toast1);
-            toastObj.show();
-      } 
-  } 
+            row.childNodes[8].innerHTML = '<button class="btn edit" data-bs-toggle="modal" data-bs-target="#editModal"'
+                'onclick="rowDetalis(this); showEditeValue();"><i class="fas fa-edit"></i></button>'
+                '<button class="btn delete" data-bs-toggle="modal" data-bs-target="#exampleModal"'
+                'onclick="rowDetalis(this)"><i class="fas fa-trash-alt" style="color: red "></i></button>';
+        })
+        var toast1 = document.getElementById("etoast");
+        var toastObj = new bootstrap.Toast(toast1);
+        toastObj.show();
+    }
+} 
+ 
 /*
 @showEditeValue
 ** this function is use to show value in adite page
  */
-function showEditeValue(){
-    var row = x.parentNode.parentNode;
+function showEditeValue()  {
+    var row = rowElement.parentNode.parentNode;
     document.querySelectorAll("#valid").forEach(clear=>{
-      clear.innerHTML= null ;
+    clear.innerHTML= null ;
     });
     var id = (document.getElementById("eid").value = row.childNodes[0].innerHTML);
     var name1 = (document.getElementById("ename").value = row.childNodes[1].innerHTML);
@@ -166,36 +167,44 @@ function showEditeValue(){
     var state = (document.getElementById("estate").value = row.childNodes[5].innerHTML);
     var country = (document.getElementById("ecountry").value = row.childNodes[6].innerHTML);
     var pin = (document.getElementById("epin").value = row.childNodes[7].innerHTML);
-  }
-/*
-** this porson is use to api call
- */
-var url = "http://192.168.0.107:8080/students";
-var p = {
-  method:"GET",
-  headers:{
-    "Content-Type":"application/json"
-  }
 }
 
-fetch(url,p).then(response =>{
-  return response.json();
-}).then(data => {
-  if (data.length > 0) {
-    var temp = "";
-    data.forEach((itemData) => {
-      temp += "<tr>";
-      temp += "<td>" + itemData.id + "</td>";
-      temp += "<td>" + itemData.name + "</td>";
-      temp += "<td>" + itemData.phoneNo + "</td>";
-      temp += "<td>" + itemData.email + "</td>";
-      temp += "<td>" + itemData.city + "</td>";
-      temp += "<td>" + itemData.state + "</td>";
-      temp += "<td>" + itemData.country + "</td>";
-      temp += "<td>" + itemData.pin + "</td>";
-      temp += "<td>" + '<button class="btn edit" data-bs-toggle="modal" data-bs-target="#editModal" onclick="rowDetalis(this); showEditeValue();"><i class="fas fa-edit"></i></button><button class="btn delete" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="rowDetalis(this)"><i class="fas fa-trash-alt" style="color: red "></i></button>' + "</td></tr>";
-    });
-   document.getElementById('api').innerHTML = temp;
-  }
-})
+/*
+@getValueOfApi
+** this function is use to call api and get data from api
+ */
+function getValueOfAPi() {
+    var url = "http://192.168.0.107:8080/students";
+    var param = {
+        method:"GET",
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }
+    fetch(url,param).then(response =>{
+        return response.json();
+    }).then(data => {
+        if (data.length > 0) {
+            var temp = "";
+            data.forEach((itemData) => {
+                temp += "<tr>";
+                temp += "<td>" + itemData.id + "</td>";
+                temp += "<td>" + itemData.name + "</td>";
+                temp += "<td>" + itemData.phoneNo + "</td>";
+                temp += "<td>" + itemData.email + "</td>";
+                temp += "<td>" + itemData.city + "</td>";
+                temp += "<td>" + itemData.state + "</td>";
+                temp += "<td>" + itemData.country + "</td>";
+                temp += "<td>" + itemData.pin + "</td>";
+                temp += "<td>" + '<button class="btn edit" data-bs-toggle="modal" data-bs-target="#editModal"' +
+                    'onclick="rowDetalis(this); showEditeValue();"><i class="fas fa-edit"></i></button>' +
+                    '<button class="btn delete" data-bs-toggle="modal" data-bs-target="#exampleModal"' +
+                    'onclick="rowDetalis(this)"><i class="fas fa-trash-alt" style="color: red "></i></button>' + 
+                    "</td></tr>";
+            });
+            document.getElementById('tableBody').innerHTML = temp;
+        }
+    })
+}
+
 
--- validation.js
+++ validation.js
@@ -1,182 +1,55 @@
-/*
-*@validation 
-**Add new function for validation all fild and submit button
-*/
-//validation for id
-// function valId(validid){ 
-//     var div = validid.parentNode;
-//     var p = div.querySelector("#valid");
-//     validid = validid.value;
-//     var len = document.getElementById("tableid").rows.length;
-//     var n=0;
-//     var i;
-//         for( i = 1; i < len; i++){
-//             if( validid == document.getElementById("tableid").rows[i].cells[0].innerHTML){
-//              n++;
-//              break;    
-//             }
-//         }
-//         if (n > 0) {
-//             p.innerHTML = "Duplicate data!";
-//             id = null;
-//             return false;
-//         }
-//         else if (/^[0-9]{1,9}$/.test(validid)) {
-//             p.innerHTML = null;
-//             return true;
-//         }
-//         else {
-//             p.innerHTML = "Invalid id";
-//             id = null;
-//             return false;
-//         }
-   
-// }
-/*
-@valName
-** use for name validation
-** 
-*/
-function valName(validName) {
-    var div = validName.parentNode;
-    var p = div.querySelector("#valid");
-    validName = validName.value;
-    if (/^[a-zA-Z]+([ ][a-zA-Z]+)*$/.test(validName)) {
-        p.innerHTML = null;
-        return true;
-    }
-    else {
-        p.innerHTML = "Invalid Name!";
-        return false;
-    }
-}
-/*
-@valNumber
-** use for number validation 
-*/
-function valNumber(validNumber) {
-    var div = validNumber.parentNode;
-    var p = div.querySelector("#valid");
-    validNumber = validNumber.value;
-    if (/^\d{10}$/.test(validNumber)) {
-        p.innerHTML = null;
-        return true;
-    }
-    else {
-        p.innerHTML = "Invalid Phone Number!";
-        return false;
-    }
-}
 /* 
-@vakEmail
-** use for email validation
+@isRegxValid
+** use for validation
+** Name,Mobile Number,Email,Pin
 */
-function valEmail(validEmail){
-    var div = validEmail.parentNode;
-    var p = div.querySelector("#valid");
-    validEmail = validEmail.value;
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(validEmail)) {
-        p.innerHTML = null;
+function isRegxValid(element, regx, massage) {
+    var parentDiv = element.parentNode;
+    var massageElement = parentDiv.querySelector("#valid");
+    var elementValue = element.value;
+    if (regx.test(elementValue)) {
+        massageElement.innerHTML = null
         return true;
-    }
-    else {
-        p.innerHTML = "Invalid Mail!";
+    } else {
+        massageElement.innerHTML = massage;
         return false;
-    }
-   
+    }    
 }
-/*
-@valcity
-** use for city validation
+
+/* 
+@isValuNull
+** use for validation
+** City,State,Country
 */
-function valCity(validCity){
-    var div = validCity.parentNode;
-    var p = div.querySelector("#valid");
-    validCity = validCity.value;
-    if (validCity=="") {        
-        p.innerHTML = "Enter Your City!";
+function isValueNull(element, massage) {
+    var parentDiv = element.parentNode;
+    var massageElement = parentDiv.querySelector("#valid");
+    var elementValue = element.value;
+    if(elementValue == "") {
+        massageElement.innerHTML = massage;
         return false;
-    }
-    else {
-        p.innerHTML = null;
+    } else {
+        massageElement.innerHTML = null;
         return true;
     }
-   
 }
-/*
-@valstate
-**  use for state validation
-*/
-function valState(validState){
-    var div = validState.parentNode;
-    var p = div.querySelector("#valid");
-    validState = validState.value;
-    if ( validState=="") {
-        
-        p.innerHTML = "Enter Your State!";
-        return false;
-    }
-    else {
-        p.innerHTML = null;
-        return true;
-    }
-   
-}
-/*
-@valcountyr 
-** use for country validation
- */
-function valCountry(validCountry){
-    var div = validCountry.parentNode;
-    var p = div.querySelector("#valid");
-    validCountry = validCountry.value;
-    if (validCountry =="") {
-        p.innerHTML = "Enter Your Country!";
-        return false;
-      
-    }
-    else {
-        p.innerHTML = null;
-        return true;
-    }
-   
-}
-/*
-@valpin
-** use for pin validation
- */
-function valPin(validPin){
-    var div = validPin.parentNode;
-    var p = div.querySelector("#valid");
-    validPin = validPin.value;
-    if (/^\d{5,6}$/.test(validPin)) {
-       p.innerHTML = null;
-        return true;
-    }
-    else {
-        p.innerHTML = "Enter Your Proper Pincode!";
-        return false;
-    }
-   
-}
+
+
 /*
 @valAll
 ** above all functon ar true then this function allow the submit data in table
  */
-function valAll(name,number,email,city,state,country,pin) {
-     if (valName(name) && valNumber(number) && valEmail(email) && valCity(city) && valState(state) && valCountry(country) && valPin(pin)) {
-     return true;    
-     }else {  
-       
-         return false; 
-     }
+function valAll(name, number, email, city, state, country, pin) {
+    if (isRegxValid(name, /^[a-zA-Z]+([ ][a-zA-Z]+)*$/, "Invalid Name!")
+        && isRegxValid(number, /^\d{10}$/, "Invalid Phone Number!")
+        && isRegxValid(email, /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Invalid Mail!")
+        && isValueNull(city, "Enter Your City!")
+        && isValueNull(state, "Enter Your State!")
+        && isValueNull(country, "Enter Your Country!")
+        && isRegxValid(pin, /^\d{5,6}$/, "Enter Your Proper Pincode!") ) {
+            return true;
+        } else {
+            return false;
+        }
 }
-// function manage(txt) {
-//     var bt = document.getElementById('button');
-//     if (txt.value != '') {
-//         bt.disabled = false;
-//     }
-//     else {
-//         bt.disabled = true;
-//     }
-// }    
+  
