60656469d6fb3c353d6352d6d578c121d86d01e0
SubrataOvaku <subratab@ovaku.in>
2023-03-02 20:21:35 +0530

    code optimise

--- validation.js
+++ validation.js
@@ -36,158 +36,76 @@ function valId(validid){
     }
 }
 
-/*
-@valName
-** use for  validation
-** validation : name
+/* 
+@isRegxValid
+** use for validation
+** Name,Mobile Number,Email,Pin
 */
-function valName(validName) {
-    var div = validName.parentNode;
-    var p = div.querySelector("#valName");
-    validName = validName.value;
-    if (/^[a-zA-Z]+([ ][a-zA-Z]+)*$/.test(validName)) {
-        p.innerHTML = null;
-        bt.disable = true;
+function isRegxValid(element, regx, massage) {
+    var parentDiv = element.parentNode;
+    var massageElement = parentDiv.querySelector("#valid");
+    var elementValue = element.value;
+    if (regx.test(elementValue)) {
+        massageElement.innerHTML = null
         return true;
-    }
-    else {
-        p.innerHTML = "Invalid Name!";
-        bt.disable = false;
+    } else {
+        massageElement.innerHTML = massage;
         return false;
+    }    
+}
+
+/* 
+@isValuNull
+** use for validation
+** City,State,Country
+*/
+function isValueNull(element, massage) {
+    var parentDiv = element.parentNode;
+    var massageElement = parentDiv.querySelector("#valid");
+    var elementValue = element.value;
+    if(elementValue == "") {
+        massageElement.innerHTML = massage;
+        return false;
+    } else {
+        massageElement.innerHTML = null;
+        return true;
     }
 }
 
+
 /*
-@valNumber
-** use for  validation
-** validation : phone number
-*/
-function valNumber(validNumber) {
-    var div = validNumber.parentNode;
-    var p = div.querySelector("#valNumber");
-    validNumber = validNumber.value;
-    if (/^\d{10}$/.test(validNumber)) {
-        p.innerHTML = null;
-        return true;
-    }
-    else {
-        p.innerHTML = "Invalid Phone Number!";
-        return false;
-    }
+@valAll
+** above all functon ar true then this function allow the submit data in table
+ */
+function valAll(id, name, number, email, city, state, country, pin) {
+    if (valId(id) && isRegxValid(name, /^[a-zA-Z]+([ ][a-zA-Z]+)*$/, "Invalid Name!")
+        && isRegxValid(number, /^\d{10}$/, "Invalid Phone Number!")
+        && isRegxValid(email, /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Invalid Mail!")
+        && isValueNull(city, "Enter Your City!")
+        && isValueNull(state, "Enter Your State!")
+        && isValueNull(country, "Enter Your Country!")
+        && isRegxValid(pin, /^\d{5,6}$/, "Enter Your Proper Pincode!") ) {
+            return true;
+        } else {
+            return false;
+        }
 }
-/*
-@valEmail
-** use for  validation
-** validation : email id
-*/
-function valEmail(validEmail){
-    var div = validEmail.parentNode;
-    var p = div.querySelector("#valEmail");
-    validEmail = validEmail.value;
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(validEmail)) {
-        p.innerHTML = null;
-        return true;
-    }
-    else {
-        p.innerHTML = "Invalid Mail!";
-        return false;
-    }
-   
-}
-/*
-@valCity
-** use for  validation
-** validation : City
-*/
-function valCity(validCity){
-    var div = validCity.parentNode;
-    var p = div.querySelector("#valcity");
-    validCity = validCity.value;
-    if (validCity=="") {        
-        p.innerHTML = "Enter Your City!";
-        return false;
-    }
-    else {
-        p.innerHTML = null;
-        return true;
-    }
-   
-}
-/*
-@valState
-** use for validation
-** validation : State
-*/
-function valState(validState){
-    var div = validState.parentNode;
-    var p = div.querySelector("#valState");
-    validState = validState.value;
-    if ( validState=="") {
-        
-        p.innerHTML = "Enter Your State!";
-        return false;
-    }
-    else {
-        p.innerHTML = null;
-        return true;
-    }
-   
-}
-/*
-@valcountry
-** use for  validation
-** validation : Country
-*/
-function valCountry(validCountry){
-    var div = validCountry.parentNode;
-    var p = div.querySelector("#valCountry");
-    validCountry = validCountry.value;
-    if (/^[a-zA-Z]+([ ][a-zA-Z]+)*$/.test(validCountry)) {
-       p.innerHTML = null;
-        return true;
-    }
-    else {
-        p.innerHTML = "Enter Your Country!";
-        return false;
-    }
-   
-}
-/*
-@valpin
-** use for  validation
-** validation : Pin Code
-*/
-function valPin(validPin){
-    var div = validPin.parentNode;
-    var p = div.querySelector("#valPin");
-    validPin = validPin.value;
-    if (/^\d{5,6}$/.test(validPin)) {
-        p.innerHTML = null;
-        
-        return true;
-    }
-    else {
-        p.innerHTML = "Enter your Proper Pincode!";
-        return false;
-    }
-   
-}
+  
 /*
 @valAll
 ** use for validation
 ** validation : all function are true the work on this vlidation
 */
-function valAll(id,name,number,email,city,state,country,pin) {
-     if (valId(id) && valName(name) && valNumber(number) && valEmail(email) && valCity(city) && valState(state) && valCountry(country) && valPin(pin)) {
-     return true;    
-     }else {  
-         return false; 
-     }
-}
-function valAllEdit(name,number,email,city,state,country,pin) {
-    if (valName(name) && valNumber(number) && valEmail(email) && valCity(city) && valState(state) && valCountry(country) && valPin(pin)) {        
-    return true;    
-    }else {  
-        return false; 
-    }
+function valAllEdit(name, number, email, city, state, country, pin) {
+    if (isRegxValid(name, /^[a-zA-Z]+([ ][a-zA-Z]+)*$/, "Invalid Name!")
+        && isRegxValid(number, /^\d{10}$/, "Invalid Phone Number!")
+        && isRegxValid(email, /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Invalid Mail!")
+        && isValueNull(city, "Enter Your City!")
+        && isValueNull(state, "Enter Your State!")
+        && isValueNull(country, "Enter Your Country!")
+        && isRegxValid(pin, /^\d{5,6}$/, "Enter Your Proper Pincode!") ) {
+            return true;
+        } else {
+            return false;
+        }
 }
